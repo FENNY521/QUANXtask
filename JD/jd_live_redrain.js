9ab662e4389e76c512815220be54543a8ee58b73
quanx <58097540+FENNY521@users.noreply.github.com>
2021-02-02 10:45:18 +0800

    Update jd_live_redrain.js

--- JD/jd_live_redrain.js
+++ JD/jd_live_redrain.js
@@ -2,13 +2,12 @@
 超级直播间红包雨
 每天20-23半点可领
 活动时间：2021年1月25日
-更新地址：https://raw.githubusercontent.com/shylocks/Loon/main/jd_live_redrain.js
 已支持IOS双京东账号, Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, 小火箭，JSBox, Node.js
 ============Quantumultx===============
 [task_local]
 #超级直播间红包雨
-30,31 20-23/1 25 1 * https://raw.githubusercontent.com/shylocks/Loon/main/jd_live_redrain.js, tag=超级直播间红包雨, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/master/Icon/shylocks/jd_live_redrain2.jpg, enabled=true
+30,31 20-23/1 25 1 * https://raw.githubusercontent.com/FENNY521/QUANXtask/main/jd_live_redrain.js, tag=超级直播间红包雨, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/master/Icon/shylocks/jd_live_redrain2.jpg, enabled=true
 
 ================Loon==============
 [Script]
